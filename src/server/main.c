e76eca41761e610a173884769fab9675af4ff7ac
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-12-17 10:24:20 +0100

    started server

--- src/server/main.c
+++ src/server/main.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 #include "Server.h"
+#include "../consoleManagement.h"
 
 void clientConnected(Client client)
 {
-    printf("%s@%s:Connected\n", Client_getUsername(client), Client_getIpS(client));
+    console_formatSystemForegroundMode("%s@%s", CONSOLE_COLOR_BRIGHT_BLUE, CONSOLE_FLAG_BOLD, Client_getUsername(client), Client_getIpS(client));
+    printf(":");
+    console_formatSystemForeground("Connected\n", CONSOLE_COLOR_BRIGHT_GREEN);
+    printf("\n");
 }
 void clientDisconnected(Client client)
 {
-    printf("%s@%s:Disconnected\n", Client_getUsername(client), Client_getIpS(client));
+    console_formatSystemForegroundMode("%s@%s", CONSOLE_COLOR_BRIGHT_BLUE, CONSOLE_FLAG_BOLD, Client_getUsername(client), Client_getIpS(client));
+    printf(":");
+    console_formatSystemForeground("Disconnected\n", CONSOLE_COLOR_BRIGHT_RED);
+    printf("\n");
 }
 size_t message(Client client, void *data, size_t dataSize, void *buffer)
 {
-    printf("%s@%s:%s", Client_getUsername(client), Client_getIpS(client), (char *)data);
-    for (int i = 0; i < dataSize - 1; i++)
-        ((char *)buffer)[i] = ((char *)data)[i] + 1;
-    return dataSize;
+    //TODO analyze client request
+    return 1;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    Server server = Server_create(6000);
+    if (argc < 2)
+    {
+        console_formatSystemForeground("Usage : server <port>", CONSOLE_COLOR_BRIGHT_RED);
+        return 1;
+    }
+    uint16_t port = atoi(argv[1]);
+    Server server = Server_create(port);
     if (!server)
         return 0;
     Server_onConnectedClient(server, &clientConnected);
     Server_onDisconnectedClient(server, &clientDisconnected);
     Server_onMessageRecieved(server, &message);
-    printf("Server initialized\n");
+    console_formatSystemColor("Server initialized to port %d", CONSOLE_COLOR_BRIGHT_GREEN, port);
+    printf("\n");
     Server_run(server);
     return 0;
 }
